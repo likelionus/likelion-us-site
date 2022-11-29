2d229e3db159a459015ab6e8295b3104590b4088
hjin2 <sunjin980@gmail.com>
2022-11-30 02:17:23 +0900

    첫번째 화면 수정본

--- js/count.js
+++ js/count.js
@@ -1,12 +1,14 @@
 var numAnimation = document.querySelectorAll(".count-num");
+var checkVisible = document.querySelector("section.history");
+
 console.log(numAnimation);
 
-function changeNum(idx) {
+function changeNum(idx,oneTime) {
   var num = 0;
   var intervalTime = 50;
   var targetNum = numAnimation[idx].getAttribute("data-count");
   if (targetNum >= 1000) {
-    intervalTime = 0.0000000001;
+    intervalTime = 1;
   }
   var timer = setInterval(function () {
     ++num;
@@ -16,7 +18,20 @@ function changeNum(idx) {
     }
   }, intervalTime);
 }
+var oneTime = 1 // 이벤트를 한번만 실행시키기 위해 변수설정
 
-for (var i = 0; i < numAnimation.length; i++) {
-  changeNum(i);
+
+// visible클래스가 있을때 스크롤 내릴때마다 숫자반복이 다시 시작되는 문제가 있었음
+function upNumber(){
+  if(checkVisible.classList.contains('visible')){ // visible클래스가 있고
+    if(oneTime==1){ // 실행횟수가 1일때
+    for (var i = 0; i < numAnimation.length; i++) {
+      changeNum(i,oneTime); // 숫자 변경
+    }
+  }
+    oneTime++; // visible클래스가 있어도 한번 실행하고 실행횟수를 증가시켜서 두번다시는 숫자 안올라가도록 함
+  }
 }
+
+
+window.addEventListener('scroll', upNumber);
--- js/scroll.js
+++ js/scroll.js
@@ -16,9 +16,25 @@ const observer = new IntersectionObserver((entries) => {
 
 const elementList = document.querySelectorAll(".element");
 const sbContainerList = document.querySelectorAll(".sbContainer");
-const historyList = document.querySelectorAll(".scrollHistory");
+// const historyList = document.querySelectorAll(".scrollHistory");
+
+const countUpSection = document.querySelectorAll(".history");
+
+const activity1 = document.querySelectorAll(".Ideathon");
+const activity2 = document.querySelectorAll(".Hackathon");
+const activity3 = document.querySelectorAll(".Mentoring");
+const activity4 = document.querySelectorAll(".Networking");
+
 
 // 반복문을 돌려 모든 DOM에 적용
 elementList.forEach((el) => observer.observe(el));
 sbContainerList.forEach((el) => observer.observe(el));
-historyList.forEach((el) => observer.observe(el));
+// historyList.forEach((el) => observer.observe(el));
+countUpSection.forEach((el)=>observer.observe(el));
+
+activity1.forEach((el)=>observer.observe(el));
+activity2.forEach((el)=>observer.observe(el));
+activity3.forEach((el)=>observer.observe(el));
+activity4.forEach((el)=>observer.observe(el));
+
+
