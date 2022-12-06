e57ddb9b03c1496441752d10ac6086f0e4ef1da6
hjin2 <sunjin980@gmail.com>
2022-12-06 11:57:59 +0900

    수정본

--- js/count.js
+++ js/count.js
@@ -7,7 +7,7 @@ function changeNum(idx,oneTime) {
   var intervalTime = 25;
   var targetNum = numAnimation[idx].getAttribute("data-count"); // 목표숫자들: 10,130,8400,1000
   if (targetNum > 1000) { // data-count가 1000보다 클때 올라가는 속도 지정
-    intervalTime = 5;
+    intervalTime = 5; // 속도를 맞추기위해 속도 다 다르게 지정
   }else if(targetNum > 130){
     intervalTime = 2;
   }
@@ -17,7 +17,7 @@ function changeNum(idx,oneTime) {
     intervalTime = 350;
   }
   var timer = setInterval(function () {
-    if(idx == 2){ // 학생부분
+    if(idx == 2){ // index[2]는 학생
       num += 10; // 8400명 부분은 속도를 맞추기 위해 10씩 증가시킴
     }else{
     ++num;  // 숫자 1씩 증가
@@ -33,7 +33,7 @@ function changeNum(idx,oneTime) {
 var oneTime = 1 // 이벤트를 한번만 실행시키기 위해 실행횟수 변수설정
 
 
-// visible클래스가 있을때 스크롤 내릴때마다 숫자반복이 다시 시작되는 문제가 있었음
+
 function upNumber(){
   if(checkVisible.classList.contains('visible')){ // section.history에 visible클래스가 있고
     if(oneTime==1){ // 실행횟수가 1일때
--- js/menu1.js
+++ js/menu1.js
@@ -1,6 +1,9 @@
+// header 메뉴 바 위치 이동 코드
+
+// header바 높이 측정
 var headerHeight1 = document.querySelector("#header").offsetHeight;
 
-/* 효진 */
+// 각 파트 별 최상단 위치 - header바 높이 위치로 이동
 var mission = document.querySelector("#mission").offsetTop - headerHeight1;
 var story = document.querySelector("#story").offsetTop - headerHeight1;
 var global = document.querySelector("#global").offsetTop - headerHeight1;
